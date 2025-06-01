7e18e1b2937862e10e50cf4f07a03e521c70d259
ckdev88 <ckdev88@outlook.com>
2025-06-01 13:09:32 +0200

    Release 0.2 - Big update

--- src/components/SearchSubjects.tsx
+++ src/components/SearchSubjects.tsx
@@ -1,33 +1,37 @@
-import { useState } from 'react'
-const SearchSubjects = ({ book_id, subjects }: { book_id: Book['id']; subjects: BookSubjects }) => {
-	const [showMore, setShowMore] = useState(false)
+import { useState } from "react"
+import BaseBadge from "./ui/BaseBadge"
+import { cleanIndexKey } from "../helpers/cleanInput"
 
-	function showMoreToggle() {
-		setShowMore(!showMore)
-	}
+const SearchSubjects = ({
+    book_id,
+    subjects,
+}: { book_id: Book["id"]; subjects: BookSubjects }) => {
+    const [showMore, setShowMore] = useState(false)
 
-	if (subjects === undefined) return
-	return (
-		<div className="subjects">
-			{subjects.map((subject, index) => {
-				if (showMore) {
-					return (
-						<div className="subject badge" key={'subject' + book_id + index}>
-							{subject}
-						</div>
-					)
-				} else if (index < 5) {
-					return (
-						<div className="subject badge" key={'subject' + book_id + index}>
-							{subject}
-						</div>
-					)
-				}
-			})}
-			<button onClick={() => showMoreToggle()} className="btn-text diblock">
-				{showMore ? '<' : '...'}
-			</button>
-		</div>
-	)
+    function showMoreToggle() {
+        setShowMore(!showMore)
+    }
+
+    if (subjects === undefined) return
+    return (
+        <div className="subjects">
+            {subjects.map((subject, index) => {
+                const key = cleanIndexKey("subject" + book_id, index)
+                if (showMore === true) {
+                    return <BaseBadge key={key} type="subject" text={subject} />
+                }
+                if (!showMore && index < 5) {
+                    return <BaseBadge key={key} type="subject" text={subject} />
+                }
+            })}
+            <button
+                type="button"
+                onClick={() => showMoreToggle()}
+                className="btn-text diblock"
+            >
+                {showMore ? "<" : "..."}
+            </button>
+        </div>
+    )
 }
 export default SearchSubjects
